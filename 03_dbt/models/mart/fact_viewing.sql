0d41fc6353cf5e65adce3996d302685fea29bc60
Aditya Daptardar <aditya.daptardar@gmail.com>
2026-02-15 19:31:39 +0000

    cleaning

--- 03_dbt/models/mart/fact_viewing.sql
+++ 03_dbt/models/mart/fact_viewing.sql
@@ -26,15 +26,7 @@ max_loaded AS (
 
 )
 
-SELECT
-    s.event_id,
-    s.user_id,
-    s.movie_id,
-    s.watch_start_time,
-    s.watch_duration_minutes,
-    s.device_type,
-    s.rating,
-    s.ingestion_ts
+SELECT s.*
 FROM source_data s
 
 {% if is_incremental() %}
